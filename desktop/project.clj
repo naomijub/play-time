a784b83edd43d4d0e46adb8efcc234e4e47e9162
Julia Naomi <jnaomi@thoughtworks.com>
2017-04-17 20:02:20 -0300

    test refresh plugin added

--- desktop/project.clj
+++ desktop/project.clj
@@ -1,6 +1,6 @@
 (defproject testing "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  
+  :description "Clojure for the Brave and True?"
+
   :dependencies [[com.badlogicgames.gdx/gdx "1.9.3"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.9.3"]
                  [com.badlogicgames.gdx/gdx-box2d "1.9.3"]
@@ -12,9 +12,21 @@
                  [com.badlogicgames.gdx/gdx-platform "1.9.3"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.7.0"]
-                 [play-clj "1.1.1"]]
-  
+                 [play-clj "1.1.1"]
+                 [expectations "2.2.0-beta1"]]
+
   :source-paths ["src" "src-common"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [testing.core.desktop-launcher]
-  :main testing.core.desktop-launcher)
+  :main testing.core.desktop-launcher
+  :profiles
+  {:dev {:dependencies [[proto-repl "0.3.1"]]
+         :plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]}}
+  :test-refresh {:growl false
+                 :notify-on-success false
+                 :quiet true
+                 :changes-only true
+                 :stack-trace-depth nil
+                 :run-once false
+                 :watch-dirs ["src" "src-common" "test-common"]
+                 :refresh-dirs ["src" "src-common" "test-common"]})
