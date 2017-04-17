11cd4b3e8b14789c734212423bb05b1b7f3a8e42
Julia Naomi <jnaomi@thoughtworks.com>
2017-04-17 18:59:40 -0300

    added logo

--- desktop/src-common/testing/core.clj
+++ desktop/src-common/testing/core.clj
@@ -1,12 +1,13 @@
 (ns testing.core
   (:require [play-clj.core :refer :all]
-            [play-clj.ui :as ui]))
+            [play-clj.ui :as ui]
+            [play-clj.g2d :as g2d]))
 
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage))
-    (ui/label "Clojure shall conquer the world!!!" (color :white)))
+    (g2d/texture "logo.png"))
 
   :on-render
   (fn [screen entities]
